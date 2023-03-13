e4753293a8fb41cd9a5e2ebc42980da71b032e88
Matheus Rodrigues de Mello <matheus_usagui@hotmail.com>
2023-03-13 17:22:48 -0300

    Updates

--- src/Components/Helper/Pagina404.tsx
+++ src/Components/Helper/Pagina404.tsx
@@ -1,10 +1,29 @@
+import { useNavigate } from 'react-router-dom';
+import React from 'react';
+
 const Pagina404 = () => {
+  const navigate = useNavigate();
+
+  React.useEffect(() => {
+    let wait = false;
+    function navFeed() {
+      navigate('/');
+    }
+    if (!wait) {
+      setTimeout(navFeed, 2000);
+      wait = true;
+    }
+    setTimeout(() => {
+      wait = false;
+    }, 500);
+  }, [navigate]);
+
   return (
     <section>
-      <h1>Erro: 404</h1>
-      <p>Página não encontrada</p>
+      <h1 className="title">Erro: 404</h1>
+      <p className="sub-title">Página não encontrada</p>
     </section>
-  )
-}
+  );
+};
 
-export default Pagina404
+export default Pagina404;
